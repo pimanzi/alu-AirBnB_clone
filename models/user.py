43c13ab81bc20141bdcb4b82e98d21015e61aba5
kimanziplacide <p.imanzi@alustudent.com>
2024-05-16 19:28:09 +0200

    pushing the rest of the code of our application

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This script is for Basemodel"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """Class for managing user objects"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
